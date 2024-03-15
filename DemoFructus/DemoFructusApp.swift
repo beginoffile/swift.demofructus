3d08413a8681b8a862da1eb91248519c93c705b6
cesar <cursos.mapatupisaro@gamail.com>
2024-03-14 18:01:22 -0600

    Initial Commit

---
+++ DemoFructus/DemoFructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFructusApp.swift
+//  DemoFructus
+//
+//  Created by Aguid Ramirez Sanchez on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoFructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
